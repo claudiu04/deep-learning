4cb894d5eff23f2ddaa100d7d6f06052d3ce02cf
Marek Kolodziej <mkolod@gmail.com>
2017-12-03 16:11:59 -0800

    Model trains and unit tests pass

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -4,6 +4,7 @@ import sys
 class NeuralNetwork(object):
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         # Set number of nodes in input, hidden and output layers.
+        np.random.seed(123)
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
