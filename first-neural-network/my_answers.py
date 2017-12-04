3f486a1458c6291f588c33518e979a2d4186ec01
Marek Kolodziej <mkolod@gmail.com>
2017-12-04 08:07:40 -0800

    Problem 1 Submission

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -31,8 +31,6 @@ class NeuralNetwork(object):
         
         '''
 
-#        features = np.array(features, ndmin=2)
-#        targetss = np.array(targets, ndmin=2)
         if len(features.shape) == 1:
             features = features[:, None]
         if len(targets.shape) == 1:
@@ -86,71 +84,19 @@ class NeuralNetwork(object):
         X = np.array(X, ndmin=2)
         y = np.array(y, ndmin=2)
         hidden_outputs = np.array(hidden_outputs, ndmin=2)
-    
-        # Add dimension if y is 1-dimensional
-#        yy = y[:, None] if len(y.shape) == 1 or len(y.shape) < len(final_outputs.shape) else y
-#        fo = final_outputs[:, None] if len(final_outputs.shape) == 1 else final_outputs 
-#        X2 = X[None, :] if len(X.shape) == 1 else X
-#        ho = hidden_outputs[None, :] if len(hidden_outputs.shape) == 1 else hidden_outputs
-
-        yy = y
-        fo = final_outputs
-        X2 = X
-        ho = hidden_outputs
  
-        error = yy - fo
-
+        error = y - final_outputs
         output_error_term = error
 
         hidden_error = np.dot(output_error_term, self.weights_hidden_to_output.T)
-
         hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
 
-        try:
-        
-            # Weight step (input to hidden)
-            delta_weights_i_h += np.dot(X2.T, hidden_error_term)
+        # Weight step (input to hidden)
+        delta_weights_i_h += np.dot(X.T, hidden_error_term)
 
-            # Weight step (hidden to output)
-            delta_weights_h_o += np.dot(ho.T, output_error_term)
+        # Weight step (hidden to output)
+        delta_weights_h_o += np.dot(hidden_outputs.T, output_error_term)
 
-#        self.input_nodes = input_nodes
-#        self.hidden_nodes = hidden_nodes
-#        self.output_nodes = output_nodes
-#
-#        # Initialize weights
-#        self.weights_input_to_hidden = np.random.normal(0.0, self.input_nodes**-0.5,
-#                                       (self.input_nodes, self.hidden_nodes))
-#
-#        self.weights_hidden_to_output = np.random.normal(0.0, self.hidden_nodes**-0.5,
-#                                       (self.hidden_nodes, self.output_nodes))
-
-
-        except ValueError as ve:
-            s = ve.__str__() + "\n"
-            s = s + "y.shape = %s\n" % str(y.shape)
-            s  = s + "yy.shape = %s\n" % str(yy.shape)
-            s = s + "final_outputs.shape = %s\n" % str(final_outputs.shape)
-            s = s + "fo.shape = %s\n" % str(fo.shape)
-            s = s + "output_error_term.shape = %s\n" % str(output_error_term.shape)
-            s = s + "hidden_error.shape = %s\n" % str(hidden_error.shape)
-            s = s + "hidden_error_term.shape = %s\n" % str(hidden_error_term.shape)
-            s = s + "X.T.shape = %s\n" % str(X.T.shape)
-            s = s + "delta_weights_i_h += np.dot(X.T, hidden_error_term)\n"
-            s = s + "self.input_nodes = %s\n" % str(self.input_nodes)
-            s = s + "self.hidden_nodes = %s\n" % str(self.hidden_nodes)
-            s = s + "self.weights_input_to_hidden.shape = %s\n" % str(self.weights_input_to_hidden.shape)
-            s = s + "self.weights_hidden_to_output.shape = %s\n" % str(self.weights_hidden_to_output.shape)
-            s = s + "delta_weights_i_h.shape = %s\n" % str(delta_weights_i_h.shape)
-            s = s + "delta_weights_h_i.shape = %s" % str(delta_weights_h_o.shape)
-
-            raise ValueError(s)
-
-
-        # TODO: Update weights
-#        delta_weights_i_h = delta_weights_i_h
-#        delta_weights_h_o = delta_weights_h_o        
-       
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
