14e5ae45e37bb48bc695696463c2950036358483
Marek Kolodziej <mkolod@gmail.com>
2018-01-21 13:38:26 -0800

    Fix Unicode issues during data loading (UTF-8 decode)

--- tv-script-generation/helper.py
+++ tv-script-generation/helper.py
@@ -7,8 +7,8 @@ def load_data(path):
     Load Dataset from File
     """
     input_file = os.path.join(path)
-    with open(input_file, "r") as f:
-        data = f.read()
+    with open(input_file, "rb") as f:
+        data = f.read().decode('utf-8')
 
     return data
 
---
+++ tv-script-generation/notebook_start.sh
@@ -0,0 +1,1 @@
+source /root/miniconda3/bin/activate dog-project && conda info --envs && jupyter notebook --allow-root --NotebookApp.token='' --ip='0.0.0.0'
